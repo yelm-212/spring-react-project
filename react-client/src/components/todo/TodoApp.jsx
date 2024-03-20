632be24b2ea982bd1f59e2c90904a4b4a361e5ed
yelm <21yrshin@naver.com>
2024-03-20 23:32:41 +0900

    style: bootstrap 적용

--- react-client/src/components/todo/TodoApp.jsx
+++ react-client/src/components/todo/TodoApp.jsx
@@ -12,7 +12,7 @@ export default function TodoApp(){
                     <Route path='/' element= { <LoginComponent/> } />
                     <Route path='/login' element= { <LoginComponent/> } />
                     <Route path='/welcome/:username' element= { <WelcomeComponent/> } />
-                    <Route path='/todos/:username' element= { <ListTodosComponent/> } />
+                    <Route path='/todos' element= { <ListTodosComponent/> } />
                     <Route path='/logout' element= { <LogOutComponent/> } />
                     <Route path='*' element= { <ErrorComponent/> } />
                 </Routes>
@@ -103,7 +103,7 @@ function LoginComponent(){
 function WelcomeComponent(){
 
     const {username} = useParams();
-    const link = `/todos/${username}`
+    const link = `/todos`
 
     return(
         <div className="WelcomeComponent">
@@ -128,20 +128,26 @@ function HeaderComponent(){
     //TODO: 이거 Home 경로를 로그인했으면 `/welcome/${username}` 으로 이동하게 하려면 어떻게 할지 생각해보기
 
     return(
-        <header className='header'>
-            <div className='container'>
-                <ul className='navbar-nav'>
-                    <li className='nav-item'>
-                        <a className='nav-link' href='https://yelm-212.github.io/'>
-                            Yelm-212
-                        </a>
-                        <Link className='nav-link' To='/'>
-                            Home
-                        </Link>
-                    </li>
-                </ul>
+        <header className="border-bottom border-light border-5 mb-5 p-2">
+        <div className="container">
+            <div className="row">
+                <nav className="navbar navbar-expand-lg">
+                    <a className="navbar-brand ms-2 fs-2 fw-bold text-black" href='https://yelm-212.github.io/'>Yelm-212</a>
+                    <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav">
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/welcome/username">Home</Link></li>
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/todos">Todos</Link></li>
+                        </ul>
+                    </div>
+                    <ul className="navbar-nav">
+                        <li className="nav-item fs-5"><Link className="nav-link" to="/login">Login</Link></li>
+                        <li className="nav-item fs-5"><Link className="nav-link" to="/logout">Logout</Link></li>
+                    </ul>
+                </nav>
             </div>
-        </header>
+        </div>
+    </header>
+
     )
 }
 
