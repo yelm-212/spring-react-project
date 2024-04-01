25503928b7d1ea161fb38968c346ad413bf5d996
yelm <21yrshin@naver.com>
2024-04-01 17:25:14 +0900

    feat: added authContext @ FooterComponent

--- react-client/src/components/todo/FooterComponent.jsx
+++ react-client/src/components/todo/FooterComponent.jsx
@@ -1,4 +1,11 @@
+import { AuthContext } from './security/AuthContext';
+import { useContext } from 'react';
+
 function FooterComponent(){
+    const authContext = useContext(AuthContext);
+
+    console.log(` Footer component : ${authContext.number} `);
+
     return(
         <div className="FooterComponent">
             <footer className='footer'>
--- react-client/src/components/todo/security/AuthContext.js
+++ react-client/src/components/todo/security/AuthContext.js
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const AuthContext = createContext();
 
 export default function AuthProvider({ children }) {
-    const [number, setNumber] = useState(0);
+    const [number, setNumber] = useState(10);
     const [username, setUsername] = useState('username');  // 초기값을 'username'으로 설정
 
     return (
