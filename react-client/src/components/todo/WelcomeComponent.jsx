e4d3c404c57dcf98aac786d9b84bf5e30557c56a
yelm <21yrshin@naver.com>
2024-04-08 17:09:24 +0900

    feat: test Axios Implementation

--- react-client/src/components/todo/WelcomeComponent.jsx
+++ react-client/src/components/todo/WelcomeComponent.jsx
@@ -1,5 +1,6 @@
 import { useParams, Link } from 'react-router-dom'
 import { useAuth } from './security/AuthContext'
+import axios from 'axios'
 
 function WelcomeComponent(){
 
@@ -8,11 +9,30 @@ function WelcomeComponent(){
 
     const link = `/todos/${username}`
 
+    function callHelloWorldApi(){
+        axios.get('http://localhost:8080/hello')
+        .then( (response) => successfulResponse(response) )
+        .catch( (error) => errorResponse(error) )
+        .finally( () => console.log('cleanup'))
+
+    }
+
+    function successfulResponse(response){
+        console.log(response)
+    }
+
+    function errorResponse(error){
+        console.log(error)
+    }
+
     return(
         <div className="WelcomeComponent">
             <h1> Welcome! {username} </h1>
             <div>
                 Manage your todos <Link to={link}>Link</Link>
+            </div>
+            <div>
+                <button className="btn btn-success m-5" onClick={callHelloWorldApi}> Hello World! </button>
             </div>
         </div>
     )
